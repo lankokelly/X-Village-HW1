47a577e81879fcd0c4936613e1cab277292d2854
lankokelly <lankokelly@gmail.com>
2018-07-15 02:04:21 +0800

    can output a matrix but without fun()

--- matrix.py
+++ matrix.py
@@ -1,7 +1,5 @@
 import random
-
 from copy import deepcopy
-
 
 class Matrix:
 
@@ -28,3 +26,27 @@ class Matrix:
     def display(self):
         """Display the content in the matrix"""
         pass
+#精簡版
+import random
+m=4
+n=2
+k=m
+list1=[[1]*n]
+list2=[1]*n
+
+for i in range (1,n+1):
+    list1[0][i-1]=random.randint(0,9)
+
+while (m>1):
+    list1.append(list2)  
+    m-=1
+
+for i in range(1,k):
+    list1[i]=[1]*k
+    for j in range (0,k):
+        list1[i][j]= random.randint(0,9)
+
+for i in range(0,k):
+    for j in range(0,n):
+        print (list1[i][j],sep=" ",end=" ")
+    print ("")
