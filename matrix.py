eea030576da6d93cd124d415119fafbdd8405a15
lankokelly <lankokelly@gmail.com>
2018-07-15 15:45:36 +0800

    can return matrix

--- matrix.py
+++ matrix.py
@@ -1,4 +1,5 @@
 import random
+import copy
 from copy import deepcopy
 class Matrix:
     def __init__(self, mrows, ncols):
@@ -12,16 +13,13 @@ class Matrix:
         
         for i in range ( 1, self.n+1 ):
             list1[0][i-1] = random.randint(0,9)
-
         while (k>1):
             list1.append( list2 )  
             k -= 1
-       
         for i in range ( 1, self.m ):
             list1[i] = [1] * self.n
             for j in range ( 0, self.n ):
                 list1[i][j] = random.randint(0,9)
- 
         for i in range( 0, self.m ):
             for j in range( 0, self.n ):
                 print (list1[i][j], sep=" " , end=" ")
@@ -33,21 +31,27 @@ class Matrix:
     def add(self, m):
         """return a new Matrix object after summation"""
         print("========== A + B ==========")
+        add_matrix = copy.deepcopy(m)
         for i in range( 0, self.m ):
             for j in range( 0, self.n ):
-                print (self.list[i][j] + m.list[i][j], sep=" " , end=" ")
+                add_matrix.list[i][j] = self.list[i][j] + m.list[i][j]
+                print (add_matrix.list[i][j], sep=" " , end=" ")
             print ("")
-        print ("")  
+        print ("")
+        return add_matrix
         pass
     
     def sub(self, m):
         """return a new Matrix object after substraction"""
         print("========== A - B ==========")
+        sub_matrix = copy.deepcopy(m)
         for i in range( 0, self.m ):
             for j in range( 0, self.n ):
-                print (self.list[i][j] - m.list[i][j], sep=" " , end=" ")
+                sub_matrix.list[i][j] = self.list[i][j] - m.list[i][j]
+                print (sub_matrix.list[i][j], sep=" " , end=" ")
             print ("")  
         print ("") 
+        return sub_matrix
         pass
 
 a_rows = input ( "Enter A matrix's rows : ")
@@ -61,4 +65,6 @@ print ("Matrix B (" , b_rows, "," , b_cols, ") :")
 B = Matrix ( b_rows , b_cols )
 
 C = A.add(B)
+print (type(C))
 D = A.sub(B)
+print (type(D))
