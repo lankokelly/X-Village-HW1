fabd357e3c5dd505acb33a1ca7a03f1ff549cede
lankokelly <lankokelly@gmail.com>
2018-07-15 15:21:56 +0800

    fix bugs of init matrix & finish ctor,add,sub

--- matrix.py
+++ matrix.py
@@ -1,51 +1,64 @@
 import random
 from copy import deepcopy
-
 class Matrix:
+    def __init__(self, mrows, ncols):
+        """Construct a (mrows X ncols) matrix"""
+        self.m = int( mrows )
+        self.n = int( ncols )
+        k = self.m
+        
+        list1 = [ [1] * self.n ]
+        list2 = [1] * self.n
+        
+        for i in range ( 1, self.n+1 ):
+            list1[0][i-1] = random.randint(0,9)
 
-    def __init__(self, nrows, ncols):
-        """Construct a (nrows X ncols) matrix"""
+        while (k>1):
+            list1.append( list2 )  
+            k -= 1
+       
+        for i in range ( 1, self.m ):
+            list1[i] = [1] * self.n
+            for j in range ( 0, self.n ):
+                list1[i][j] = random.randint(0,9)
+ 
+        for i in range( 0, self.m ):
+            for j in range( 0, self.n ):
+                print (list1[i][j], sep=" " , end=" ")
+            print ("")
+        print ("")  
+        self.list = list1
         pass
 
     def add(self, m):
         """return a new Matrix object after summation"""
-        pass
-
-    def sub(self, m):
-        """return a new Matrix object after substraction"""
-        pass
-
-    def mul(self, m):
-        """return a new Matrix object after multiplication"""
-        pass
-
-    def transpose(self):
-        """return a new Matrix object after transpose"""
+        print("========== A + B ==========")
+        for i in range( 0, self.m ):
+            for j in range( 0, self.n ):
+                print (self.list[i][j] + m.list[i][j], sep=" " , end=" ")
+            print ("")
+        print ("")  
         pass
     
-    def display(self):
-        """Display the content in the matrix"""
+    def sub(self, m):
+        """return a new Matrix object after substraction"""
+        print("========== A - B ==========")
+        for i in range( 0, self.m ):
+            for j in range( 0, self.n ):
+                print (self.list[i][j] - m.list[i][j], sep=" " , end=" ")
+            print ("")  
+        print ("") 
         pass
 
-m=4
-n=2
-k=m
-list1=[[1]*n]
-list2=[1]*n
+a_rows = input ( "Enter A matrix's rows : ")
+a_cols = input ( "Enter A matrix's cols : ")
+print ("Matrix A (" , a_rows, "," , a_cols, ") :")
+A = Matrix ( a_rows , a_cols )
 
-for i in range (1,n+1):
-    list1[0][i-1]=random.randint(0,9)
+b_rows = input("Enter B matrix's rows : ")
+b_cols = input("Enter B matrix's cols : ")
+print ("Matrix B (" , b_rows, "," , b_cols, ") :")
+B = Matrix ( b_rows , b_cols )
 
-while (m>1):
-    list1.append(list2)  
-    m-=1
-
-for i in range(1,k):
-    list1[i]=[1]*k
-    for j in range (0,k):
-        list1[i][j]= random.randint(0,9)
-
-for i in range(0,k):
-    for j in range(0,n):
-        print (list1[i][j],sep=" ",end=" ")
-    print ("")
+C = A.add(B)
+D = A.sub(B)
